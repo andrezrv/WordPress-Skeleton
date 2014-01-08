f85a1f6110e6da3e45743de109b2d5d273829041
andrezrv <andrezrv@gmail.com>
2014-01-08 01:20:53 -0300

    Added comments and debug settings.

--- local-config-sample.php
+++ local-config-sample.php
@@ -1,12 +1,22 @@
 <?php
-/*
-This is a sample local-config.php file
-In it, you *must* include the four main database defines
+/**
+ * This is a sample local-config.php file
+ * In it, you *must* include the four main database defines
+ * 
+ * You may include other settings here that you only want enabled on your local development checkouts
+ */
 
-You may include other settings here that you only want enabled on your local development checkouts
-*/
-
+// ==================
+// Load database info
+// ==================
 define( 'DB_NAME', 'local_db_name' );
 define( 'DB_USER', 'local_db_user' );
 define( 'DB_PASSWORD', 'local_db_password' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+
+// ==============
+// Debug settings
+// ==============
+ini_set( 'display_errors', 0 ); // Set this to 1 if you want to see the whole bunch of PHP errors.
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_DISPLAY', true );
